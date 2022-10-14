c2d754793d93c3c4b470d6f441fe87d75bc80143
pepe_fabra_valverde <jofaval@outlook.com>
2022-10-14 16:15:01 +0200

    fix(ci/cd): Use proper `publicDir` on prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+const base =
+  process.env.NODE_ENV === "production" ? "./astro-list-dynamic-test/" : "./";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base,
+});
